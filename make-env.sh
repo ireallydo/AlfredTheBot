28d2806e14c0fad27b2deec5b56726befac22449
Irina Dovzhenok <93725678+ireallydo@users.noreply.github.com>
2023-05-15 14:15:54 +0200

    Update make-env.sh

--- make-env.sh
+++ make-env.sh
@@ -2,7 +2,7 @@
 
 cat > ./wc_testing/.env <<EOF
 
-TG_TOKEN = 6163709410:AAGvZrCpDRBXwGW6bxF4CYpbxVYQvj5jbPQ
-OPENAI_TOKEN = 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
+TG_TOKEN = somestring
+OPENAI_TOKEN = somestring
 
 EOF
